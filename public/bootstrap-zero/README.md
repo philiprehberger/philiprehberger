d6ffc0fe0fd22c619ce903f170dc9fad19a9659f
philiprehberger <me@philiprehberger.com>
2014-07-20 16:11:56 -0600

    public folder

---
+++ public/bootstrap-zero/README.md
@@ -0,0 +1,82 @@
+bootstrap-zero
+==============
+
+Bootstrap Templates and Themes Project
+----
+
+- Free, open source Bootstrap templates and themes for Twitter Bootstrap.
+- App written on Node.js
+- Complete HTML, CSS and JavaScript
+- Demo and downloads: http://www.bootstrapzero.com
+
+
+![Screenshot](https://pbs.twimg.com/media/Bc6QcqoCcAAPFCQ.png:large)
+
+A Repository for Bootstrap Templates
+---
+
+- reusable
+- responsive (suitable for mobile devices)
+- follow best practices
+- maximize use of the Bootstrap baseline
+- aren't extensively customized
+- don't look to Bootstrap-y
+
+Standard Templates
+----
+    
+Basic, sidebar, affix, off-canvas, carousel, gallery, fixed width, fixed navbar, blog, portal, app and others.
+
+
+Custom Templates and Themes
+----
+    
+Facebook style, Google+ style, Android Holo-style, Mashable-style, TheNextWeb style, off-canvas, one page app, long scrolling, full screen carousel and others.
+
+License
+---
+[MIT](https://github.com/iatek/bootstrap-zero/blob/master/LICENSE) for this project. Individual licenses apply to templates and themes.
+
+
+Contribute
+---
+If you author open source Bootstrap templates or themes you can submit your works for inclusion on BootstrapZero. Template should meet this criteria:
+
+- free, open source
+- no use of LESS or custom Bootstrap builds
+- 100% responsive for multiple device widths
+
+While a database is in the works, here is how you can submit your project(s)..
+
+- Construct a JSON object for each template/theme you want to submit. Here is an example:
+
+    ```
+    {
+        name:"Darkside",
+        title:"Darkside",
+        short:"Dark with off-canvas sidebar",
+        full:"The deep gray layout features a sidebar drawer menu that responsively collapses off-canvas on smaller devices such as tablets and smartphones.<br>When the sidebar is collapsed, it can be toggled on so that it slides in from the left and consumes 40% of the screen width.<br>This template also includes a sticky bottom footer that stays at the bottom regardless of content height.",
+        tags:["theme","fixed","dark","collapse","off-canvas"],
+        imgUrl:"http://www.bootply.com/assets/templates/88105.png",
+        downloadUrl:"http://bootply.com/download/88105",
+        sourceUrl:"http://bootply.com/88105",
+        previewUrl:"http://bootply.com/render/88105",
+        author:"Bootply.com",
+        authorUrl:"http://www.bootply.com/templates",
+        bootstrapVersion:"3.0.3",
+        avatar:"https://pbs.twimg.com/profile_images/3663020003/d09fae59ab68605a7973043e0267b905.jpeg",
+        license:"MIT",
+        licenseUrl:"https://github.com/iatek/bootstrap-zero/blob/master/LICENSE"
+    }
+    ```
+- Fork this project
+- Append your template/theme info the 'templates.js' file found in the project
+- Submit a pull request so that we can review your submission
+
+
+
+
+
+
+
+
---
+++ public/bootstrap-zero/server.js
@@ -0,0 +1,146 @@
+var port = process.env.PORT || 4000,
+    app = require('./app').init(port),
+    utils = require('./utils'),
+    conf = require('./conf'),
+    request = require('request'),
+    pageSize = 20;
+   
+    
+app.get('/', function(req, res){
+
+    var templates = require('./templates');
+    var starters = [], themes = [];
+    
+    for (var i=0;i<templates.length;i++) {
+    
+        if (templates[i].tags.indexOf("starter")!=-1) {
+            starters.push(templates[i]);    
+        }
+        
+        if (templates[i].tags.indexOf("theme")!=-1) {
+            themes.push(templates[i]);  
+        } 
+    }
+    
+    res.render("index",{templates:templates,starters:starters,themes:themes,path:"/"});
+    
+});
+
+app.get('/templates/:id', function(req, res){
+
+    console.log("/templates.......");
+
+    var templates = require('./templates');
+    var id = req.params.id;
+   
+    if (typeof id==="undefined") {
+        return;
+    }
+    
+    for (var i=0;i<templates.length;i++) {
+    
+        var prev = (templates[i-1])||templates[templates.length-1];
+        var next = (templates[i+1])||templates[0];
+    
+        if (templates[i].id===id) {
+            res.render("detail",{template:templates[i],next:next,prev:prev,title:templates[i].title+" Bootstrap template",desc:"Bootstrap " + templates[i].title + " template example. This is a free, responsive starter template and theme for Bootstrap 3 from BootstrapZero."});
+            return;
+        }    
+        
+    }
+    
+    res.render("404",{error:"no results"});
+    
+});
+
+app.get('/bootstrap-template/:title', function(req, res){
+
+    console.log("/template by title.......");
+
+    var templates = require('./templates');
+    var title = req.params.title;
+   
+    if (typeof title==="undefined") {
+        return;
+    }
+    else {
+        title = title.replace(/-/g," ");
+    }
+    
+    for (var i=0;i<templates.length;i++) {
+    
+        var prev = (templates[i-1])||templates[templates.length-1];
+        var next = (templates[i+1])||templates[0];
+    
+        if (templates[i].title.toLowerCase()===title.toLowerCase()) {
+            res.render("detail",{template:templates[i],next:next,prev:prev,title:"Bootstrap "+title+" template",desc:"Bootstrap " + title + " template example. This is a free, responsive starter template and theme for Bootstrap 3 from BootstrapZero.",keywords:"bootstrap,"+templates[i].tags+",free,bootstrap template"});
+            return;
+        }    
+        
+    }
+    
+    res.render("404",{error:"no results"});
+    
+});
+
+app.get('/api/templates', function(req, res){
+    var fs = require('fs');
+    
+   //fs.readFile('./templates.json', 'utf8', function (err, data) {
+    //    res.json(data);
+    //});
+    
+    var data = require('./templates');
+    res.json(data);
+    
+    //var fileJSON = require('./static/templates.json');
+    //res.send(fileJSON);
+    
+    //res.writeHead(200, {"Content-Type": "application/json"});
+    //fs.createReadStream('templates.json',{flags:'r',encoding:'utf-8'}).pipe(res);
+    
+});
+
+app.get('/bootstraps/:page', function(req, res){
+
+    console.log("/templates paged list all.......");
+    
+    var page = (req.params.page)||1;
+    var templates = require('./templates').reverse();
+    templates = templates.slice();
+    
+    
+    res.render("list",{templates:templates,tag:"Bootstrap",title:"Free bootstrap themes and templates",desc:"Bootstrap templates and examples. A collection of free, responsive starter templates and themes for Bootstrap from BootstrapZero."});
+});
+
+app.get('/bootstrap-templates', function(req, res){
+
+    console.log("/templates list all.......");
+    
+    var templates = require('./templates').reverse();
+    
+    res.render("list",{templates:templates,tag:"Bootstrap",title:"Free bootstrap themes and template",desc:"Bootstrap templates and examples. A collection of free, responsive starter templates and themes for Bootstrap from BootstrapZero."});
+});
+
+app.get('/bootstrap-templates/:tag', function(req, res){
+
+    var templates = require('./templates');
+    var tag = req.params.tag;
+    var tagged = [];
+    
+    for (var i=0;i<templates.length;i++) {
+    
+        if (templates[i].tags.indexOf(tag)!=-1) {
+            tagged.push(templates[i]);    
+        }
+    }
+    
+    res.render("list",{templates:tagged,tag:tag,title:'Bootstrap ' + tag.replace("theme","") + ' themes and templates - BootstrapZero'});
+    
+});
+
+/* The 404 Route (ALWAYS Keep this as the last route) */
+app.get('/*', function(req, res){
+    res.render('404.ejs');
+});
+
